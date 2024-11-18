bede2eec84f579325447738f4e03612e4cd23a1f
Cyril Levis <git@levis.name>
2024-11-18 18:26:29 +0100

    chore: add flake.nix

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  inputs = {
+    naersk.url = "github:nix-community/naersk/master";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, naersk }:
+    let systems = [ "x86_64-linux" "aarch64-linux" ];
+    in flake-utils.lib.eachSystem systems (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        naersk-lib = pkgs.callPackage naersk { };
+        libs = with pkgs; [
+          udev
+          dbus
+          openssl
+        ];
+      in
+      {
+        defaultPackage = naersk-lib.buildPackage {
+          src = ./.;
+          meta.mainProgram = "shelly-waybar";
+          nativeBuildInputs = with pkgs; [
+            makeWrapper
+            rustPlatform.bindgenHook
+            marked-man
+          ];
+          buildInputs = libs;
+        };
+        devShell = with pkgs; mkShell {
+          buildInputs = [ cargo rustc rustfmt pre-commit rustPackages.clippy pkg-config ] ++ libs;
+          RUST_SRC_PATH = rustPlatform.rustLibSrc;
+        };
+      }
+    );
+}
